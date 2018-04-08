75fdf4911fda3d77ed449b27bd48df99b7cb2a74
sankar basu <nemo8130@gmail.com>
2018-04-08 09:33:00 +0530

    Update chromnum.m

--- chromnum.m
+++ chromnum.m
@@ -40,7 +40,8 @@ function [crn,colour,order] = find_chromatic_number(adj)
 %
 %
 % Authors: Abhirup Bandyopadhyay and Sankar Basu 
-% 
+%
+% * Author for all Correspondence
 % Email: Sankar Basu (nemo8130@gmail.com)
 % Abhirup Bandyopadhyay (abhirupnit@gmail.com)
 % 
